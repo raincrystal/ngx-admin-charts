69d7d875a9a723f70a7f4e808126a4626153e333
Rain <rain.crystal@outlook.com>
2020-03-27 09:38:27 +0100

    Fix chart type

--- src/app/components/charts/echarts-line.component.ts
+++ src/app/components/charts/echarts-line.component.ts
@@ -84,17 +84,20 @@ export class EchartsLineComponent implements AfterViewInit, OnDestroy {
           {
             name: 'Line 1',
             type: 'line',
-            data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669],
+            smooth: true,
+            data: [6669, 3, 741, 247, 81, 247, 741, 2223, 6669],
           },
           {
             name: 'Line 2',
             type: 'line',
-            data: [1, 2, 4, 8, 16, 32, 64, 128, 256],
+            smooth: true,
+            data: [1, 128, 32, 256, 16, 32, 64, 128, 256],
           },
           {
             name: 'Line 3',
             type: 'line',
-            data: [1 / 2, 1 / 4, 1 / 8, 1 / 16, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 512],
+            smooth: true,
+            data: [1 / 512, 1 / 4, 1 / 8, 1 / 128, 1 / 32, 1 / 64, 1 / 128, 1 / 256, 1 / 8],
           },
         ],
       };
